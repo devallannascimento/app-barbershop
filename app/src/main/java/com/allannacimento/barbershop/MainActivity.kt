0c5ba4ad1a571232cf6213582ad76c9cb6099c8a
devallannascimento <allancontatopessoal@gmail.com>
2023-07-12 20:08:35 -0300

    Tela de Login finalizada

--- app/src/main/java/com/allannacimento/barbershop/MainActivity.kt
+++ app/src/main/java/com/allannacimento/barbershop/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.allannacimento.barbershop
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.allannacimento.barbershop.databinding.ActivityMainBinding
+import com.allannacimento.barbershop.view.Home
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        binding.btnLogin.setOnClickListener {
+
+            val nome = binding.editNome.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            when {
+                nome.isEmpty() -> {
+                    mensagem(it, "Coloque o seu nome!")
+                }
+
+                senha.isEmpty() -> {
+                    mensagem(it, "Preencha a senha!")
+                }
+
+                senha.length <= 5 -> {
+                    mensagem(it, "A senha precisa ter pelo menos 6 caracteres!")
+                }
+
+                else -> {
+                    navegarParaHome(nome)
+                }
+            }
+
+        }
+
     }
+
+    private fun mensagem(view: View, mensagem: String) {
+        val snackbar = Snackbar.make(view, mensagem, Snackbar.LENGTH_SHORT)
+        snackbar.setBackgroundTint(Color.parseColor("#FF0000"))
+        snackbar.setTextColor(Color.parseColor("#FFFFFF"))
+        snackbar.show()
+    }
+
+    private fun navegarParaHome(nome: String) {
+        val intent = Intent(this, Home::class.java)
+        intent.putExtra("nome", nome)
+        startActivity(intent)
+    }
+
+
 }
---
+++ app/src/main/java/com/allannacimento/barbershop/view/Home.kt
@@ -0,0 +1,12 @@
+package com.allannacimento.barbershop.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.allannacimento.barbershop.R
+
+class Home : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
